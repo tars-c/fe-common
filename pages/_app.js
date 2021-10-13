4fd4bca4aebfe422dcc7350907a4ccab1a525ba2
edit8080 <leetaehee0205@gmail.com>
2021-10-13 17:52:17 +0900

    feat : redux-saga 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import 'public/styles/global.sass'
+import React from 'react'
+
+import { applyMiddleware, createStore } from 'redux'
+
+import createSagaMiddleware from '@redux-saga/core'
+import rootReducer, { rootSaga } from '@modules/index'
+import { Provider } from 'react-redux'
+
+// saga 미들웨어 연결
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(rootSaga)
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
