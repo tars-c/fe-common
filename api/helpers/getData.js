4ed6c9b0135cadbbbb6bb27809dd6995789374cb
edit8080 <leetaehee0205@gmail.com>
2021-10-13 17:49:32 +0900

    chore : 파일명 수정

---
+++ api/helpers/getData.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const getData = async (path) => {
+  try {
+    if (!path) return
+
+    const data = await axios.get(`http://49.50.167.136:9871/api${path}`)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export default getData
