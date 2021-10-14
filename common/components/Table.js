c7d3dae6e42d98a58197687e84610f9141d18dc1
edit8080 <leetaehee0205@gmail.com>
2021-10-14 14:05:29 +0900

    feat : Table JSX 구조 생성

---
+++ common/components/Table.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Table = ({ categories, dataList }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {categories.map((category, idx) => (
+            <th key={`thead__${idx}`}>{category.value}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {dataList.map((data, dIdx) => {
+          return (
+            <tr key={`trow__${dIdx}`}>
+              {categories.map((category, cIdx) => (
+                <td key={`tdata__${cIdx}`}>{data[category.id]}</td>
+              ))}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
+
+Table.propTypes = {
+  categories: PropTypes.array,
+  dataList: PropTypes.array,
+}
+Table.defaultProps = {
+  categories: [],
+  dataList: [],
+}
+
+export default Table
