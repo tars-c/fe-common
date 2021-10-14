ff24cc6bee2c0d531c6c2382d6e7134fff5b2a81
edit8080 <leetaehee0205@gmail.com>
2021-10-14 11:04:54 +0900

    chore : api response를 destructure한 후 사용

--- api/helpers/getData.js
+++ api/helpers/getData.js
@@ -4,8 +4,8 @@ const getData = async (path) => {
   try {
     if (!path) return
 
-    const response = await axios.get(`http://49.50.167.136:9871/api${path}`)
-    return response.data
+    const { data } = await axios.get(`http://49.50.167.136:9871/api${path}`)
+    return data
   } catch (error) {
     console.log(error)
   }
