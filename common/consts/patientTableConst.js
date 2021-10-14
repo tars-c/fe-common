9c53471ddbc690ee6265a4c5e0fa5f176428b075
edit8080 <leetaehee0205@gmail.com>
2021-10-14 14:06:39 +0900

    feat : patientTable 컨테이너에서 사용할 상수와 필터 함수 선언

---
+++ common/consts/patientTableConst.js
@@ -0,0 +1,8 @@
+export const patientCategories = [
+  { id: 'age', value: '나이' },
+  { id: 'birthDatetime', value: '생년월일' },
+  { id: 'ethnicity', value: '민족' },
+  { id: 'gender', value: '성별' },
+  { id: 'isDeath', value: '사망여부' },
+  { id: 'race', value: '인종' },
+]
