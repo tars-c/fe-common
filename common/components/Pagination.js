2248a3fb36cef60fb7f9c88c46ef3eee4a22674f
edit8080 <leetaehee0205@gmail.com>
2021-10-15 13:06:17 +0900

    style : 패이지네이션 색에 대해 style 변수 추출

--- common/components/Pagination.js
+++ common/components/Pagination.js
@@ -2,6 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+import { PAGINATION_COLOR } from '@common/styles/variables'
+
 const WrapPagination = styled.div`
   display: flex;
 `
@@ -9,14 +11,14 @@ const WrapPaginationBtn = styled.a`
   display: block;
   margin: 15px 3px 0;
   padding: 10px;
-  border: 1px solid #33658a;
-  background-color: ${({ selected }) => (selected ? '#33658a' : 'white')};
-  color: ${({ selected }) => (selected ? 'white' : '#33658a')};
+  border: 1px solid ${PAGINATION_COLOR};
+  background-color: ${({ selected }) => selected && `${PAGINATION_COLOR}`};
+  color: ${({ selected }) => (selected ? 'white' : `${PAGINATION_COLOR}`)};
   cursor: pointer;
   border-radius: 5px;
 
   &:hover {
-    background-color: #33658a;
+    background-color: ${PAGINATION_COLOR};
     color: white;
   }
 `
