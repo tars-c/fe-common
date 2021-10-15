83057c80a3a77d71f21e7b8ae8b597de8eae20f4
edit8080 <leetaehee0205@gmail.com>
2021-10-15 14:45:53 +0900

    feat : 환자 ID 컬럼 추가 및 컬럼 순서 변경

--- patient/consts/patientTableConst.js
+++ patient/consts/patientTableConst.js
@@ -1,10 +1,13 @@
+export const initListFetch = ['race', 'gender', 'ethnicity']
+
 export const patientCategories = [
-  { id: 'age', value: '나이', tableCol: null },
-  { id: 'birthDatetime', value: '생년월일', tableCol: 'birth' },
-  { id: 'ethnicity', value: '민족', tableCol: 'ethnicity' },
+  { id: 'personID', value: '환자 ID', tableCol: 'person_id' },
   { id: 'gender', value: '성별', tableCol: 'gender' },
-  { id: 'isDeath', value: '사망여부', tableCol: 'death' },
+  { id: 'birthDatetime', value: '생년월일', tableCol: 'birth' },
+  { id: 'age', value: '나이', tableCol: null },
   { id: 'race', value: '인종', tableCol: 'race' },
+  { id: 'ethnicity', value: '민족', tableCol: 'ethnicity' },
+  { id: 'isDeath', value: '사망여부', tableCol: 'death' },
 ]
 export const paginationOpts = [
   { text: '10개씩 출력', value: 10 },
