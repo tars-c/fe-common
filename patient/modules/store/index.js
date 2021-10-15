406dc1db96e7c7e05601880bf02a478a57a83e90
edit8080 <leetaehee0205@gmail.com>
2021-10-15 16:46:53 +0900

    feat : 테이블 필터에 대한 스토어 구성

--- patient/modules/store/index.js
+++ patient/modules/store/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 
 import pagination from '@patient/modules/store/pagination'
+import filter from '@patient/modules/store/filter'
 
-const patient = combineReducers({ pagination })
+const patient = combineReducers({ pagination, filter })
 
 export default patient
