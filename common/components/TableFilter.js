9d3c2838ecdbc0a206ffa09c22f856eafb51d6e3
edit8080 <leetaehee0205@gmail.com>
2021-10-15 18:16:13 +0900

    chore : 필터 컴포넌트 구성 수정

--- common/components/TableFilter.js
+++ common/components/TableFilter.js
@@ -2,15 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { FaFilter } from 'react-icons/fa'
 
-const TableFilter = ({ onClick }) => {
-  return <FaFilter onClick={onClick} />
+const TableFilter = ({ id, onClick }) => {
+  return (
+    <span id={id} onClick={onClick}>
+      <FaFilter />
+    </span>
+  )
 }
 
 TableFilter.propTypes = {
+  id: PropTypes.string,
   onClick: PropTypes.func,
 }
 
 TableFilter.defaultProps = {
+  id: '',
   onClick: () => {},
 }
 
