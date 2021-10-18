9801f1c330c9bfed1cc35ab50672539307f69c5d
edit8080 <leetaehee0205@gmail.com>
2021-10-18 16:46:11 +0900

    feat : redux-saga를 활용한 환자 상세 정보 비동기 데이터 상태 관리

--- modules/index.js
+++ modules/index.js
@@ -4,6 +4,7 @@ import { all } from 'redux-saga/effects'
 import api from '@api/modules/store'
 import patient from '@patient/modules/store'
 import { fetchDataSaga } from '@api/modules/saga/fetchData'
+import { fetchDetailSaga } from '@patient/modules/saga/fetchDetail'
 
 const rootReducer = combineReducers({
   api,
@@ -11,7 +12,7 @@ const rootReducer = combineReducers({
 })
 
 export function* rootSaga() {
-  yield all([fetchDataSaga()])
+  yield all([fetchDataSaga(), fetchDetailSaga()])
 }
 
 export default rootReducer
--- patient/modules/store/index.js
+++ patient/modules/store/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux'
 
+import detail from '@patient/modules/store/detail'
 import pagination from '@patient/modules/store/pagination'
 import filter from '@patient/modules/store/filter'
 
-const patient = combineReducers({ pagination, filter })
+const patient = combineReducers({ pagination, filter, detail })
 
 export default patient
