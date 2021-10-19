4d3af7896c99bd92bd785c2f5c073de3fdd4ffa0
edit8080 <leetaehee0205@gmail.com>
2021-10-19 09:47:31 +0900

    style : 링크 아이콘 추가 및 일부 스타일 변경

--- patient/containers/PatientDetailContainer.js
+++ patient/containers/PatientDetailContainer.js
@@ -1,22 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { BiLinkExternal } from 'react-icons/bi'
+
+const WrapDetailInfo = styled.tr`
+  text-align: center;
+`
+const WrapDetailList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`
+const WrapDetailPageLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    color: #3388aa;
+    cursor: pointer;
+  }
+`
 
 const PatientDetailContainer = ({ detail }) => {
   return (
-    <tr onClick={(e) => e.stopPropagation()}>
-      <td>
+    <WrapDetailInfo onClick={(e) => e.stopPropagation()}>
+      <td colSpan="100%">
         <h2>
           방문 횟수 : <span>{detail.visitCount}</span>
         </h2>
         <h2>진단 정보</h2>
-        <ul>
-          {detail.conditionList.map((d, idx) => (
-            <li key={`detailCondition__${idx}`}>{d}</li>
-          ))}
-        </ul>
-        <div>상세 페이지 바로가기</div>
+        <WrapDetailPageLink>
+          <BiLinkExternal />
+          상세 페이지 바로가기
+        </WrapDetailPageLink>
       </td>
-    </tr>
+    </WrapDetailInfo>
   )
 }
 
